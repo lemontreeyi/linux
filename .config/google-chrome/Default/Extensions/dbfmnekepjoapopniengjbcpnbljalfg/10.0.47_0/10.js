c86e7833fb73d6c205d2e735f9952d5cbe599afc
yuanyi <yyi1725@foxmail>
2021-09-25 20:46:43 +0800

    added file

--- .config/google-chrome/Default/Extensions/dbfmnekepjoapopniengjbcpnbljalfg/10.0.47_0/10.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{770:function(n,i,t){"use strict";t.r(i),t.d(i,"checkOldLocalData",(function(){return c}));t(10);const c=async()=>["infinity-settings","infinity-searches","infinity-icons"].some(n=>!!localStorage.getItem(n))}}]);
